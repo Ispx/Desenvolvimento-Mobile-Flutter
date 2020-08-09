c2b861e92727339062908b96ab771e8f71b0f159
Isaque Paixão <isaquespx98@gmail.com>
2020-08-09 20:03:22 -0300

    deleted

--- byte_bank_alura/lib/theme/AppTheme.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-AppTheme() {
-  return ThemeData(
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-  );
-}
--- projeto_simples01/android/app/src/main/kotlin/com/example/projeto_simples01/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projeto_simples01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
