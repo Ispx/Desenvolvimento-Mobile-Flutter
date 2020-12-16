10b14bf50a600d54de6c768b840a27091ee8346c
Isaque Paixão <isaquespx98@gmail.com>
2020-12-16 13:52:41 -0300

    deleteds

--- byte_bank_v1/android/app/src/main/kotlin/br/com/projeto/new_byte_bank/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.projeto.new_byte_bank
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- byte_bank_v1/lib/models/contact.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-class Contact {
-  int id;
-  String nome;
-  int conta;
-  Contact({this.id, this.nome, this.conta});
-}
